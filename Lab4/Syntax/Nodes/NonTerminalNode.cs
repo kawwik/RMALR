213c71afabe16c372124a90f8962ab39dae2abc5
Mikhail <misha.rukovishnikow@yandex.ru>
2023-01-04 15:31:32 +0300

    Поддержка атрибутов в Node

--- Lab4/Syntax/Nodes/NonTerminalNode.cs
+++ Lab4/Syntax/Nodes/NonTerminalNode.cs
@@ -1,9 +1,13 @@
-﻿namespace Lab4.Syntax.Nodes;
+﻿using System.Collections.Immutable;
+
+namespace Lab4.Syntax.Nodes;
 
 public class NonTerminalNode : Node
 {
     private readonly List<Node> _children = new();
-    
+
+    private readonly Dictionary<string, dynamic> _attributes = new();
+
     public NonTerminalNode(string type)
     {
         Type = type;
@@ -11,5 +15,9 @@ public class NonTerminalNode : Node
 
     public string Type { get; }
 
+    public ImmutableDictionary<string, dynamic> Attributes => _attributes.ToImmutableDictionary();
+
     public void AddChildren(params Node[] nodes) => _children.AddRange(nodes);
+
+    public void AddAttribute(string name, dynamic value) => _attributes.Add(name, value);
 }
